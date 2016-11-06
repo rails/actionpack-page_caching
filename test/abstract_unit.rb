0a9a4d235dc68e9037213a8d33591f6c3c2e6a69
Andrew White <andrew.white@unboxed.co>
2016-11-06 21:59:08 +0000

    Run tests in random order when possible

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -5,6 +5,10 @@ require "action_controller/page_caching"
 
 SharedTestRoutes = ActionDispatch::Routing::RouteSet.new
 
+if ActiveSupport.respond_to?(:test_order)
+  ActiveSupport.test_order = :random
+end
+
 module ActionController
   class Base
     include SharedTestRoutes.url_helpers
