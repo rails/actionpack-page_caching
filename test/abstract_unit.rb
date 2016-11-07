169bcb4f3c898058611efbe44cf83e69ecc1a698
Andrew White <andrew.white@unboxed.co>
2016-11-07 01:34:10 +0000

    Fragment cache logging is turned off by default in Rails 5.1+

--- test/abstract_unit.rb
+++ test/abstract_unit.rb
@@ -8,6 +8,9 @@ if ActiveSupport.respond_to?(:test_order)
   ActiveSupport.test_order = :random
 end
 
+if ActionController::Base.respond_to?(:enable_fragment_cache_logging=)
+  ActionController::Base.enable_fragment_cache_logging = true
+end
 
 if Rails::VERSION::STRING < "4.1"
   ActionController::Renderers.add :html do |text, options|
