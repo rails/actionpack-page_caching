6191cc5540edb8cc8b68886f7637bd3726ee6cd5
Andrew White <andrew.white@unboxed.co>
2021-05-11 07:56:03 +0100

    Remove use of deprecated `URI.parser`

--- lib/action_controller/caching/pages.rb
+++ lib/action_controller/caching/pages.rb
@@ -146,7 +146,7 @@ module ActionController
             if path.empty? || path =~ %r{\A/+\z}
               name = "/index"
             else
-              name = URI.parser.unescape(path.chomp("/"))
+              name = URI::DEFAULT_PARSER.unescape(path.chomp("/"))
             end
 
             if File.extname(name).empty?
